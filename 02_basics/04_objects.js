78899bb55cdad82486be1cfe820d74d2067686fe
Kshitij singh <99023146+Kshitij321@users.noreply.github.com>
2024-01-29 20:26:05 +0530

    Update 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,5 +1,5 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+ const tinderUser = new Object()          // singleton object
+const tinderUser = {}                     // non singleton object
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
@@ -7,7 +7,7 @@ tinderUser.isLoggedIn = false
 
 // console.log(tinderUser);
 
-const regularUser = {
+const regularUser = {                      //   (object in object nesting)
     email: "some@gmail.com",
     fullname: {
         userfullname: {
@@ -17,19 +17,36 @@ const regularUser = {
     }
 }
 
-// console.log(regularUser.fullname.userfullname.firstname);
 
+
+
+
+
+ console.log(regularUser.fullname?.userfullname.firstname);         //(question mark to check if object exist? then only access)
+
+
+
+
+
+//MERGE OBJECTS--------------------------------------------------------------------------------------------
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = { obj1, obj2 }                                      (object in object problem same as array)
+// const obj3 = Object.assign({}, obj1, obj2, obj4)                 (to merge all objects together in one , see the "{}"  this is optional we assume braces as target andall other objects as source)
 
-const obj3 = {...obj1, ...obj2}
+const obj3 = {...obj1, ...obj2}                               // simple approach to merge objects spread operator
 // console.log(obj3);
 
 
+
+
+
+
+
+// IMP FUNCTIONS OF OBJECT-----------------------------------------------------------------------------------
+// Data comes from database as array of objects below
 const users = [
     {
         id: 1,
@@ -48,12 +65,21 @@ const users = [
 users[1].email
 // console.log(tinderUser);
 
+// to get keys,values,entries  (one key value as an array) as an array 
+
 // console.log(Object.keys(tinderUser));
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));                     (To ask if object has any property?)
 
+
+
+
+
+
+
+//OBJECT DESTRUCTURING------------------------------------------------------------------
 
 const course = {
     coursename: "js in hindi",
@@ -61,12 +87,24 @@ const course = {
     courseInstructor: "hitesh"
 }
 
-// course.courseInstructor
+// course.courseInstructor                     
 
-const {courseInstructor: instructor} = course
+const {courseInstructor: instructor} = course                      //(destructuring the object, means taking out value)
+                                                                  //(since, we dont want to write course.courseinstructor to acces key values)
+                                                                  //(We can give it as any name we like in above we can call "instructor")
 
 // console.log(courseInstructor);
 console.log(instructor);
+
+
+
+
+
+
+
+
+//JSON--------------------------------------------------------------
+// (This is how we get response from APIS, in object form or array form)
 
 // {
 //     "name": "hitesh",
