3bb4d14596c699e8f55d5a970405ce2c8ae2d808
Kshitij singh <99023146+Kshitij321@users.noreply.github.com>
2024-01-30 23:47:01 +0530

    Update 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,4 +1,5 @@
-//var c = 300
+//var c = 300                        (we cant declare variables with "var" as when variable defined with var works as global variable)
+                                     //(global and local scope is different in node environment and browser)
 let a = 300
 if (true) {
     let a = 10
@@ -14,6 +15,18 @@ if (true) {
 // console.log(c);
 
 
+
+
+
+
+
+
+
+
+
+
+
+// BASICS ONLY..................................................
 function one(){
     const username = "hitesh"
 
@@ -41,10 +54,17 @@ if (true) {
 // console.log(username);
 
 
+
+
+
+
+
+
+
 // ++++++++++++++++++ interesting ++++++++++++++++++
 
-
-console.log(addone(5))
+// Two ways to declare variable
+console.log(addone(5))              //(If function declared without variable then we can call it even from before declaration)
 
 function addone(num){
     return num + 1
@@ -52,7 +72,7 @@ function addone(num){
 
 
 
-addTwo(5)
+addTwo(5)                          //(if we declare function with variable then we cant call it before its declaration)
 const addTwo = function(num){
     return num + 2
 }
