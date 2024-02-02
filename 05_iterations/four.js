67f8fb2323ac80d1f3cafc287b130f0958e4c0a9
Kshitij singh <99023146+Kshitij321@users.noreply.github.com>
2024-02-02 23:51:52 +0530

    Update four.js

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,3 +1,5 @@
+//(FOR IN LOOP)-----------------------------------------------------------------------------------
+
 const myObject = {
     js: 'javascript',
     cpp: 'C++',
@@ -5,15 +7,22 @@ const myObject = {
     swift: "swift by apple"
 }
 
-for (const key in myObject) {
+
+
+for (const key in myObject) {                                                                   //(Traverse the object)
     //console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
+
+
 const programming = ["js", "rb", "py", "java", "cpp"]
 
-for (const key in programming) {
+for (const key in programming) {                                             //(Traverse the array, it gives indexes when we print key and programming[key] gives actual value)
     //console.log(programming[key]);
 }
+
+
+
 
 // const map = new Map()
 // map.set('IN', "India")
@@ -21,6 +30,6 @@ for (const key in programming) {
 // map.set('Fr', "France")
 // map.set('IN', "India")
 
-// for (const key in map) {
+// for (const key in map) {                                                     // (for in loop) does not work for map
 //     console.log(key);
 // }
