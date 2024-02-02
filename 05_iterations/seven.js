a1fd49ddae332ef044bcc390fb9c8d7ce0858e39
Kshitij singh <99023146+Kshitij321@users.noreply.github.com>
2024-02-03 00:30:29 +0530

    Update seven.js

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,7 +1,11 @@
+//Map method --------------------------------------------------------------------------------------------------------------
+//(it returns values)
 const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNumers.map( (num) => { return num + 10})
 
+
+//Chaining different methods together,2nd chained method gets value from first method
 const newNums = myNumers
                 .map((num) => num * 10 )
                 .map( (num) => num + 1)
