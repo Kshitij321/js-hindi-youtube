692e1a3f5816f94120899f7d704058fb00cde080
Kshitij singh <99023146+Kshitij321@users.noreply.github.com>
2024-02-14 00:59:57 +0530

    Update Prototype.js

--- 10_classes_and_oop/Prototype.js
+++ 10_classes_and_oop/Prototype.js
@@ -1,3 +1,5 @@
+//#3
+
 // let myName = "hitesh     "
 // let mychannel = "chai     "
 
@@ -16,8 +18,8 @@ let heroPower = {
     }
 }
 
-Object.prototype.hitesh = function(){
-    console.log(`hitesh is present in all objects`);
+Object.prototype.hitesh = function(){                                                        //we injected the hitesh function in the object hence all objects 
+    console.log(`hitesh is present in all objects`);                                         //like array,objects etc. will have access to this function
 }
 
 Array.prototype.heyHitesh = function(){
@@ -29,7 +31,14 @@ Array.prototype.heyHitesh = function(){
 // myHeros.heyHitesh()
 // heroPower.heyHitesh()
 
+
+
+
+
+
 // inheritance
+
+
 
 const User = {
     name: "chai",
@@ -45,19 +54,19 @@ const TeachingSupport = {
 }
 
 const TASupport = {
-    makeAssignment: 'JS assignment',
-    fullTime: true,
-    __proto__: TeachingSupport
+    makeAssignment: 'JS assignment',                       //we use __proto__ to link 2 objects together or to make one object inherit properties of other object
+    fullTime: true,                                        //This is old style code ,now we do this with ".prototype" just as seen before
+    __proto__: TeachingSupport                             // "TASupport" has access to all properties of "TeachingSupport"
 }
 
-Teacher.__proto__ = User
+Teacher.__proto__ = User                                   // we can give access to properties to in this way also outside
 
 // modern syntax
-Object.setPrototypeOf(TeachingSupport, Teacher)
+Object.setPrototypeOf(TeachingSupport, Teacher)            //NEW way
 
 let anotherUsername = "ChaiAurCode     "
 
-String.prototype.trueLength = function(){
+String.prototype.trueLength = function(){                  // inecting new method "trueLength" in String
     console.log(`${this}`);
     console.log(`True length is: ${this.trim().length}`);
 }
