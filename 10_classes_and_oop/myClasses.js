b2bf9d774e10ab991b5686309b3f59770a8c4653
Kshitij singh <99023146+Kshitij321@users.noreply.github.com>
2024-02-14 01:52:58 +0530

    Update myClasses.js

--- 10_classes_and_oop/myClasses.js
+++ 10_classes_and_oop/myClasses.js
@@ -1,8 +1,10 @@
+//#6
+
 // ES6
 
 class User {
-    constructor(username, email, password){
-        this.username = username;
+    constructor(username, email, password){                                 //here we dont need to declare variables explicitly
+        this.username = username;                                           // since we defined constructor function JS knows which varibales are used
         this.email = email;
         this.password = password
     }
@@ -23,7 +25,7 @@ console.log(chai.changeUsername());
 
 // behind the scene
 
-function User(username, email, password){
+function User(username, email, password){                              // this is called a constructor function these are specially used to create a new object ONLY
     this.username = username;
     this.email = email;
     this.password = password
