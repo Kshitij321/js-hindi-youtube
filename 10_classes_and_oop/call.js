73755fd67a16580141b1339c09cfe91049aaca56
Kshitij singh <99023146+Kshitij321@users.noreply.github.com>
2024-02-14 01:30:34 +0530

    Update call.js

--- 10_classes_and_oop/call.js
+++ 10_classes_and_oop/call.js
@@ -1,8 +1,15 @@
+//#4
+
 function SetUsername(username){
     //complex DB calls
     this.username = username
     console.log("called");
 }
+
+
+//call method is used to call a method ,where we can pass an object to tell from which object this function will be called
+// other paramas will be params given to the function (if function takes parameter)
+// apply() method does same thing as call() just for the function parameters it takes values enclosed in "[]";
 
 function createUser(username, email, password){
     SetUsername.call(this, username)
@@ -11,5 +18,5 @@ function createUser(username, email, password){
     this.password = password
 }
 
-const chai = new createUser("chai", "chai@fb.com", "123")
+const chai = new createUser("chai", "chai@fb.com", "123")  //creating new object by calling a function
 console.log(chai);
