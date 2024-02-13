c5cb798b8519db54daab393bc262f88568dc550b
Kshitij singh <99023146+Kshitij321@users.noreply.github.com>
2024-02-14 02:20:11 +0530

    Update staticprop.js

--- 10_classes_and_oop/staticprop.js
+++ 10_classes_and_oop/staticprop.js
@@ -1,3 +1,5 @@
+//#8
+
 class User {
     constructor(username){
         this.username = username
@@ -7,13 +9,19 @@ class User {
         console.log(`Username: ${this.username}`);
     }
 
-    static createId(){
-        return `123`
+    static createId(){                                           //static methods are methods which are of class and not for an instance of class
+        return `123`                                             // hence they can only be called using class name 
     }
 }
 
 const hitesh = new User("hitesh")
 // console.log(hitesh.createId())
+
+
+
+
+
+
 
 class Teacher extends User {
     constructor(username, email){
