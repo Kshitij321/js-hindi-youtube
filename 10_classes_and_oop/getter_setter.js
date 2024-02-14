cb801893ff984a9685aa33a5e582cf514699b73d
Kshitij singh <99023146+Kshitij321@users.noreply.github.com>
2024-02-14 17:20:55 +0530

    Update getter_setter.js

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -1,3 +1,7 @@
+//#11
+
+// in js we use set ang get keyword for making getter and setter functions for a variable in class,both must be present not single,name should be same as variable
+
 class User {
     constructor(email, password){
         this.email = email;
@@ -5,7 +9,7 @@ class User {
     }
 
     get email(){
-        return this._email.toUpperCase()
+        return this._email.toUpperCase()                                //it returns the email even if inside it is written "_email" since function name is same
     }
     set email(value){
         this._email = value
